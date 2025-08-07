f127cc1c4b3547b1a0abb0249d914f9c70e1700f
Saksham Sawhney <123295305+SakshamSawhney@users.noreply.github.com>
2025-08-08 00:44:54 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import { DataProvider } from './contexts/DataContext.tsx' // <-- ADDED
 import { Toaster } from 'react-hot-toast'
 import { ThemeProvider } from './contexts/ThemeContext.tsx'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <AuthProvider>
-          <App />
-          <Toaster position="top-center" reverseOrder={false} />
+          {/* DataProvider is wrapped by AuthProvider to access the user */}
+          <DataProvider> 
+            <App />
+            <Toaster position="top-center" reverseOrder={false} />
+          </DataProvider>
         </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
